5fff2776482f27efac45371362ec86c14a55943a
Rafael <rafael_breno@outlook.com>
2022-08-19 00:48:38 -0300

    feat: Add packages and set Global Logger

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/fiuskylab/docker-api
 
 go 1.19
+
+require go.uber.org/zap v1.22.0
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	zapLogger, err := zap.NewProduction()
+	// Application MUSTN'T start without
+	// log setup.
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// ReplaceGlobals isn't recommended, but in this
+	// project, I'm using it to avoid injecting it
+	// in every struct and/or passing it as param.
+	undoLogger := zap.ReplaceGlobals(zapLogger)
+
+	defer undoLogger()
 }
