63ffd248e588074346dc46b383c8b0ffcd87f5ff
Rafael <rafael_breno@outlook.com>
2022-08-23 21:36:04 -0300

    feat: Add basic ContainerRequest entity

--- entity/container.go
+++ entity/container.go
@@ -1,51 +1,27 @@
 package entity
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
+import "fmt"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-)
+// ContainerRequest - A data model to represent the
+// Docker Container request entity.
+type ContainerRequest struct {
+	// Name represents the name that will
+	// be assigned to the container.
+	// Optional.
+	Name string `json:"name"`
+	// ImageName represents the name of the
+	// image to be pulled.
+	// Required value.
+	ImageName string `json:"image_name"`
+	// Entrypoint - The container entry point.
+	// Optional.
+	Entrypoint []string `json:"entrypoint"`
+}
 
-func f() {
-	ctx := context.Background()
-	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-
-	if err != nil {
-		panic(err)
+// Validate - will validate all ContainerRequest's fields.
+func (c *ContainerRequest) Validate() error {
+	if c.ImageName == "" {
+		return fmt.Errorf(`the field '%s' is required`, "image_name")
 	}
-
-	imageName := "hello-world"
-
-	out, err := dockerClient.ImagePull(ctx, imageName, types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	defer out.Close()
-
-	io.Copy(os.Stdout, out)
-
-	resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
-		Image: imageName,
-	}, nil, nil, nil, "")
-	if err != nil {
-		panic(err)
-	}
-
-	if err = dockerClient.
-		ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
-	}
-
-	b, err := json.MarshalIndent(resp, "", "	")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(b))
+	return nil
 }
