688170b8adc99ca1f87c419d98f6cdf676dffc86
Rafael <rafael_breno@outlook.com>
2022-08-23 23:03:17 -0300

    feat: Add Container entity to handle Docker actions

--- entity/container.go
+++ entity/container.go
@@ -1,6 +1,12 @@
 package entity
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
 
 // ContainerRequest - A data model to represent the
 // Docker Container request entity.
@@ -27,4 +33,30 @@ func (c *ContainerRequest) Validate() error {
 		return fmt.Errorf(`the field '%s' is required`, "image_name")
 	}
 	return nil
+}
+
+// ToContainer will build an instance of Container.
+func (c *ContainerRequest) ToContainer() *Container {
+	envs := []string{}
+	for k, v := range c.Env {
+		envs = append(envs, fmt.Sprintf("%s=%s", k, v))
+	}
+	return &Container{
+		Name: c.Name,
+		Config: &container.Config{
+			Image:      c.ImageName,
+			Entrypoint: c.Entrypoint,
+			Env:        envs,
+		},
+	}
+}
+
+// Container - A data model to store all DockerContainer
+// necessary data.
+type Container struct {
+	Config           *container.Config
+	HostConfig       *container.HostConfig
+	NetworkingConfig *network.NetworkingConfig
+	Platform         *v1.Platform
+	Name             string
 }
