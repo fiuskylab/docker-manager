8eee802840cd97607563e1112aa7ab9f8c76580d
Rafael <rafael_breno@outlook.com>
2022-08-23 22:07:31 -0300

    feat: Add Logger

--- main.go
+++ main.go
@@ -9,20 +9,36 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/client"
+	"github.com/fiuskylab/docker-api/entity"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	// should not run if log is not set
+	if err != nil {
+		panic(err)
+	}
+
+	// replacing global logger, is not recommended
+	// but for this project I'll do it to avoid
+	// dependencies management related to log.
+	undo := zap.ReplaceGlobals(logger)
+
+	defer undo()
+
 	ctx := context.Background()
 	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	imageName := "hello-world"
+	cont := entity.ContainerRequest{
+		ImageName: "hello-world",
+	}
 
-	out, err := dockerClient.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	out, err := dockerClient.ImagePull(ctx, cont.ImageName, types.ImagePullOptions{})
 	if err != nil {
 		panic(err)
 	}
@@ -32,8 +48,8 @@ func main() {
 	io.Copy(os.Stdout, out)
 
 	resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
-		Image:      imageName,
-		Entrypoint: strslice.StrSlice{},
+		Image:      cont.ImageName,
+		Entrypoint: cont.Entrypoint,
 	}, nil, nil, nil, "")
 	if err != nil {
 		panic(err)
